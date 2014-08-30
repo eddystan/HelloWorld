0728e6dd17df2b03bb65c3acc4ee215fa36e2273
eddystan <psalm236>
2014-08-30 17:50:02 +0800

    Initial commit

--- app/src/main/java/app/helloworld/ifolgestudio/com/helloworld/MyActivity.java
+++ app/src/main/java/app/helloworld/ifolgestudio/com/helloworld/MyActivity.java
@@ -17,7 +17,7 @@ public class MyActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
+        // Inflate the menu; this adds items to the action bar if it is present...
         getMenuInflater().inflate(R.menu.my, menu);
         return true;
     }
@@ -28,9 +28,11 @@ public class MyActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+        //if (id == R.id.action_settings) {
+        //    return true;
+        //}
+        //return super.onOptionsItemSelected(item);
+        return id == R.id.action_settings || super.onOptionsItemSelected(item);
+
     }
 }
